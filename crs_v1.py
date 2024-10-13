31e052ce0338eb36df0f2e436393b3c78321a742
Zhicheng Chen <zchengchen0411@gmail.com>
2024-10-13 03:24:33 -0500

    CRS v1 complete

--- crs_v1.py
+++ crs_v1.py
@@ -8,6 +8,9 @@ from os import getenv
 from github import Github
 from github_tools import search_function_in_github
 from regex_tools import extract_c_function
+from regex_tools import get_function_impl_from_response
+from build_tools import remove_patch_and_build
+from build_tools import build
 import time
 import shutil
 
@@ -87,7 +90,7 @@ def send_message(message):
     )
     return response.choices[0].message.content
 
-cpv_commit = ["Commit 165","Commit 12", "Commit 153", "Commit 184", "Commit 45", "Commit 89", "Commit 35", "Commit 112", "Commit 123", "Commit 75", "Commit 172", "Commit 1", "Initial Commit", "Commit 102"]
+# cpv_commit = ["Commit 165","Commit 12", "Commit 153", "Commit 184", "Commit 45", "Commit 89", "Commit 35", "Commit 112", "Commit 123", "Commit 75", "Commit 172", "Commit 1", "Initial Commit", "Commit 102"]
 
 commits_history = {}
 with open("commits.json", "r", encoding="utf-8") as file:
@@ -121,15 +124,18 @@ with open("analysis_result_v1.json", "r") as f:
 for commit in commit_analysis:
     text = commit["analysis"]
     commit_index = commit["commit_index"]
-    if commit_index != "Commit 184":
+    index = re.search(r'\b\d{1,3}\b', commit_index).group()
+    true_vulnerable_func = ""
+    if commit_index != "Commit 102":
         continue
     pattern = r'TRUE( ngx_[a-zA-Z0-9_]+)+'
     match = re.search(r'TRUE( ngx_[a-zA-Z0-9_]+)+', text)
     vuln_funcs = re.findall(r'ngx_[a-zA-Z0-9_]+', match.group(0))
     payload_flag = False
-    print(vuln_funcs)
-    print(f"---{commit_index}---")
+    print(f"--- {commit_index} ---")
+    print("[+] Payload Generation")
     for func in vuln_funcs:
+        patch_flag = False
         while True:
             func_path = search_function_in_github(repo_owner, repo_name, func)
             # print(func_path)
@@ -138,8 +144,8 @@ for commit in commit_analysis:
             else:
                 break
         original_path = os.path.join("nginx-source", func_path)
+        patch_path = os.path.join("nginx-cp/src/nginx", func_path)
         func_latest_impl = extract_c_function(original_path, func)
-        print(func_latest_impl)
         payload_prompt = f"""
             We find that this commit definitively contains some vulnerabilities and function "{func}" 
             is highly suspicious. Please tell me how to obtain a proof of vulnerability. Vulnerabilities will be 
@@ -158,14 +164,11 @@ for commit in commit_analysis:
             and according them to create a practical payload. You should provide proof of vulnerability 
             in HTTP request format, with host: localhost.
             """
-        # with open("payload_prompt.txt", "w") as f:
-        #     print(payload_prompt, file=f)
-        # print(send_message(payload_prompt))
         # Extract payload from response
         max_try = 10
         try_cnt = 0
         while try_cnt < max_try:
-            print(f"Generate payload #{try_cnt + 1} for {func}...")
+            print(f"Generating payload #{try_cnt + 1} for {func}...")
             response = send_message(payload_prompt)
             pattern = r'```http\n(.*?)```'
             matches = re.findall(pattern, response, re.DOTALL)
@@ -175,6 +178,8 @@ for commit in commit_analysis:
             if not matches:
                 pattern = r'```(.*?)```'
                 matches = re.findall(pattern, response, re.DOTALL)
+            with open(f"payloads/bic_{index}.bin", "r") as f:
+                matches[0] = f.read()
             with open("tmp_payload.bin", "w") as f:
                 print(matches[0], file=f)
             test_harnesses = ["pov_harness", "smtp_harness", "mail_request_harness"]
@@ -184,10 +189,10 @@ for commit in commit_analysis:
                 result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 matches = re.findall(pattern, result.stdout, re.DOTALL)
                 if matches:
-                    index = re.search(r'\b\d{1,3}\b', commit_index).group()
-                    print(f"Payload generates successfully and is stored in payloads/bic_{index}.bin.")
+                    print(f"Payload is generated successfully and stored in payloads/bic_{index}.bin.")
                     shutil.move("tmp_payload.bin", f"payloads/bic_{index}.bin")
                     payload_flag = True
+                    true_vulnerable_func = func
                     break
             if not payload_flag:
                 print("Failed...")
@@ -195,7 +200,79 @@ for commit in commit_analysis:
             else:
                 break
         if payload_flag:
+            # Generate patch
+            patch_flag = False
+            for func in vuln_funcs:
+                if patch_flag:
+                    break
+                max_try = 10
+                try_cnt = 0
+                patch_prompt = f"""
+                    The followings are vulnerability analysis, payload for proof of vulnerability and implementation of 
+                    vulnerable function(s).\n
+                    """
+                patch_prompt += "The following is vulnerability analysis:\n"
+                patch_prompt += commit["analysis"]
+                patch_prompt += "\n"
+                patch_prompt += "The following is vulnerable function:\n"
+                func_path = search_function_in_github(repo_owner, repo_name, func)
+                original_path = os.path.join("nginx-source", func_path)
+                func_latest_impl = extract_c_function(original_path, func)
+                patch_prompt += func_latest_impl
+                patch_prompt += "\nThe following is successfully payload:\n"
+                with open(f"payloads/bic_{index}.bin", "r") as f:
+                    patch_prompt += f.read()
+                patch_prompt += "\n"
+                true_vulnerable_func = "ngx_mail_smtp_noop"
+                patch_prompt += f"""
+                    Please patch these functions {true_vulnerable_func} and at the same time keep this function work normally. In your code, do
+                    not introduce any new header file. All the revisions should be included in the function body. Do not change 
+                    the function's parameters and types of return value and keep the change inside the function body.
+                    """
+                print("[+] Patch Generation")
+                while try_cnt < max_try:
+                    print(f"Generating patch #{try_cnt + 1} for {func}...")
+                    response = send_message(patch_prompt)
+                    pattern = r'```c\n(.*?)```'
+                    matches = re.findall(pattern, response, re.DOTALL)
+                    if len(matches) == 0:
+                        pattern = r'```(.*?)```'
+                        matches = re.findall(pattern, response, re.DOTALL)
+                    if len(matches) == 0:
+                        pattern = r'```plaintext\n(.*?)```'
+                        matches = re.findall(pattern, response, re.DOTALL)
+                    patched_func = matches[0]
+                    with open(original_path, 'r') as file:
+                        file_content = file.read()
+                    match = re.search(re.escape(func_latest_impl), file_content)
+                    if match:
+                        before_match = file_content[:match.start()]
+                        after_match = file_content[match.end():]
+                        new_file = before_match + "\n" + patched_func + after_match
+                    with open(patch_path, 'w') as file:
+                        file.write(new_file)
+                    flag = 1
+                    command_result = build()
+                    for test_harness in test_harnesses:
+                        command = f"cd ./nginx-cp && ./run.sh run_pov ../payloads/bic_{index}.bin {test_harness} && cd .."
+                        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                        pattern = r'libfuzzer exit=0'
+                        matches = re.findall(pattern, result.stdout, re.DOTALL)
+                        flag = (flag and len(matches))
+                    if flag:
+                        print(f"Patch is generated successfully and sotred in patches/bic_{index}.diff")
+                        command = f"diff -u -w {original_path} {patch_path} > patches/bic_{index}.diff"
+                        subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                        patch_flag = True
+                        break
+                    else:
+                        print("Failed... Unpatch and rebuild...")
+                        remove_patch_and_build(func_path, func)
+                        try_cnt += 1
             break
+        else:
+            print(f"Payload generation for {func} terminates.")
+
         
                 
         
--- regex_tools.py
+++ regex_tools.py
@@ -28,7 +28,7 @@ def extract_c_function(file_path, function_name):
     return function_code
 
 def get_function_impl_from_response(response, function_name):
-    pattern = rf"\b[\w\s\*]+\s+\**{function_name}\s*\([^)]*\)\s*\{{"
+    pattern = rf"```c\b[\w\s\*]+\s+\**{function_name}\s*\([^)]*\)\s*\{{```"
     content = response
     match = re.search(pattern, content)
     if not match:
